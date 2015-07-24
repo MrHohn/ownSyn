51979c108217fbed10cd47063ba1adc2d6ec7a09
MrHohn <zzhonzi@gmail.com>
2015-07-24 19:48:35 -0400

    deleted the wordcount src

--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "wordcount.h"  
-  
-int main(int argc, char *argv[])   
-{    
-  return HadoopPipes::runTask(HadoopPipes::TemplateFactory<WordcountMapper, WordcountReducer>());    
-}    
